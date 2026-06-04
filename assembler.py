a34f4ad74875afb752dec16beb0e08236fc82d49
Guilherme <40771861+Finch404@users.noreply.github.com>
2026-06-03 23:23:11 -0300

    Add files via upload

---
+++ assembler.py
@@ -0,0 +1,249 @@
+import sys
+
+fsrc = open(str(sys.argv[1]), 'r')
+
+lines = []
+lines_bin = []
+names = []
+
+instructions = ['add', 'sub', 'mov', 'jz', 'inc', 'dec', 'zera',
+                'madd', 'msub', 'mmov', 'mjz', 'goto', 'halt', 'wb', 'ww']
+
+# x, y, mem
+instruction_set = {'add' : [0x02, 0x14],
+                   'sub' : [0x0D, 0x18],
+                   'mov' : [0x06, 0x1F],
+                   'jz'  : [0x0B, 0x22], # byte_only
+                   'inc' : [0x11, 0x1C, 0x24], # 1op 
+                   'dec' : [0x12, 0x1D, 0x27], # 1op 
+                   'zera': [0x13, 0x1E, 0x2A], # 1op
+                   'madd': [0x2D, 0x35, 0x39],
+                   'msub': [0x3D, 0x45, 0x49],
+                   'mmov': [0x4D, 0x53, 0x57],
+                   'mjz' : 0x3D, # mem1 = word ; mem2 = byte 
+                   'goto': 0x09, # byte_only
+                   'halt': 0xFF}
+
+byte_only_instructions = [instruction_set['jz'][0], instruction_set['jz'][1],
+                          instruction_set['goto']]
+
+def is_instruction(str):
+   global instructions
+   inst = False
+   for i in instructions:
+      if i == str:
+         inst = True
+         break
+   return inst
+   
+def is_name(str):
+   global names
+   name = False
+   for n in names:
+      if n[0] == str:
+         name = True
+         break
+   return name
+   
+def encode_2ops(inst, ops):
+   line_bin = []
+   if len(ops) > 1:
+      if is_name(ops[1]):
+         inst_bin = 0
+
+         if ops[0] == 'x': inst_bin = instruction_set[inst][0]
+         elif ops[0] == 'y': inst_bin = instruction_set[inst][1]
+         
+         line_bin.append(inst_bin)
+         line_bin.append(ops[1])
+         print(f"encodando {inst} {ops[0]} {ops[1]} = {inst_bin} {ops[1]}")   
+
+   return line_bin
+   
+def encode_1op(inst, ops):
+   line_bin = []
+   if len(ops) > 0:
+      if ops[0] in ['x', 'y']: #se op é registrador
+         inst_bin = 0
+         if ops[0] == 'x': inst_bin = instruction_set[inst][0]
+         elif ops[0] == 'y': inst_bin = instruction_set[inst][1]
+            
+         print(f"encodando {inst} {ops[0]} = {inst_bin}")
+         line_bin.append(inst_bin)
+
+      elif is_name(ops[0]): #se op é endereço de memória
+         inst_bin = instruction_set[inst][2]
+         print(f"encodando {inst} {ops[0]} = {inst_bin} {ops[0]}")      
+         line_bin.append(inst_bin)
+         line_bin.append(ops[0])
+   return line_bin
+
+def encode_mem_inst(inst, ops):
+   line_bin = []
+   if len(ops) > 1:
+      inst_bin = 0
+      if is_name(ops[0]):
+         if is_name(ops[1]):
+            inst_bin = instruction_set[inst][0]
+            print(f"encodando {inst} {ops[0]} {ops[1]} = {inst_bin} {ops[0]} {ops[1]}")
+            line_bin.append(instruction_set[inst][0])
+            line_bin.append(ops[0]) # word
+            line_bin.append(ops[1]) # word
+
+         elif ops[1] in ['x', 'y']:
+            if ops[1] == 'x': inst_bin = instruction_set[inst][1]
+            elif ops[1] == 'y': inst_bin = instruction_set[inst][2]
+
+            print(f"encodando {inst} {ops[0]} {ops[1]} = {inst_bin} {ops[0]}")
+            line_bin.append(inst_bin)
+            line_bin.append(ops[0]) # word
+   return line_bin
+
+def encode_mjz(ops):
+   line_bin = []
+   if len(ops) > 1:
+      if is_name(ops[0]) and is_name(ops[1]):
+         print(f"encodando mjz {ops[0]} {ops[1]} = {instruction_set['mjz']} {ops[0]} {ops[1]}")
+         line_bin.append(instruction_set['mjz'])
+         line_bin.append(ops[0]) # word
+         line_bin.append(ops[1]) # byte
+   return line_bin
+
+def encode_goto(ops):
+   line_bin = []
+   if len(ops) > 0:
+      if is_name(ops[0]):
+         print(f"encodando goto {ops[0]} = {instruction_set['goto']} {ops[0]}")
+         line_bin.append(instruction_set['goto'])
+         line_bin.append(ops[0])
+   return line_bin
+
+def encode_halt():
+   line_bin = []
+   line_bin.append(instruction_set['halt'])
+   print("halt")
+   return line_bin
+   
+def encode_wb(ops):
+   line_bin = []
+   if len(ops) > 0:
+      if ops[0].isnumeric():
+         if int(ops[0]) < 256:
+            line_bin.append(int(ops[0]))
+   return line_bin
+
+def encode_ww(ops):
+   line_bin = []
+   if len(ops) > 0:
+      if ops[0].isnumeric():
+         val = int(ops[0])
+         if val < pow(2,32):
+            line_bin.append(val & 0xFF)
+            line_bin.append((val & 0xFF00) >> 8)
+            line_bin.append((val & 0xFF0000) >> 16)
+            line_bin.append((val & 0xFF000000) >> 24)
+   return line_bin
+      
+def encode_instruction(inst, ops):
+   if inst in ['add', 'sub', 'mov', 'jz']:
+      return encode_2ops(inst, ops)
+   elif inst in ['inc', 'dec', 'zera']:
+      return encode_1op(inst, ops)
+   elif inst in ['madd', 'msub', 'mmov']:
+      return encode_mem_inst(inst, ops)
+   elif inst == 'mjz':
+      return encode_mjz(ops)
+   elif inst == 'goto':
+      return encode_goto(ops)
+   elif inst == 'halt':
+      return encode_halt()
+   elif inst == 'wb':
+      return encode_wb(ops)
+   elif inst == 'ww':
+      return encode_ww(ops)
+   else:
+      return []
+   
+   
+def line_to_bin_step1(line):
+   line_bin = []
+   if is_instruction(line[0]):
+      line_bin = encode_instruction(line[0], line[1:])
+   else:
+      line_bin = encode_instruction(line[1], line[2:])
+   return line_bin
+   
+def lines_to_bin_step1():
+   global lines
+   for line in lines:
+      line_bin = line_to_bin_step1(line)
+      if line_bin == []:
+         print("Erro de sintaxe na linha ", lines.index(line))
+         return False
+      lines_bin.append(line_bin)
+   return True
+
+def find_names():
+   global lines
+   for k in range(0, len(lines)):
+      is_label = True
+      for i in instructions:
+          if lines[k][0] == i:
+             is_label = False
+             break
+      if is_label:
+         names.append((lines[k][0], k))
+         
+def count_bytes(line_number):
+   line = 0
+   byte = 1
+   while line < line_number:
+      byte += len(lines_bin[line])
+      line += 1
+   return byte
+
+def get_name_byte(str):
+   for name in names:
+      if name[0] == str:
+         return name[1]
+         
+def resolve_names():
+   for i in range(0, len(names)):
+      names[i] = (names[i][0], count_bytes(names[i][1]))
+   for line in lines_bin:
+      for i in range(0, len(line)):
+         if is_name(line[i]):
+
+            #if line[0] == 45: #mjz mem mem
+            #   if i == 1: line[i] = get_name_byte(line[i])//4 # word
+            #   elif i == 2: line[i] = get_name_byte(line[i]) # byte
+            if i == 1 and line[0] == 9: print(9 in byte_only_instructions) 
+            if line[i-1] in byte_only_instructions:
+               line[i] = get_name_byte(line[i])
+            else:
+               print(f"{line[i]} = {get_name_byte(line[i])}")
+               line[i] = get_name_byte(line[i])//4
+
+for line in fsrc:
+   tokens = line.replace('\n','').replace(',','').lower().split(" ")
+   i = 0
+   while i < len(tokens):
+      if tokens[i] == '':
+         tokens.pop(i)
+         i -= 1
+      i += 1
+   if len(tokens) > 0:
+      lines.append(tokens)
+   
+find_names()
+if lines_to_bin_step1():
+   resolve_names()
+   byte_arr = [0]
+   for line in lines_bin:
+      for byte in line:
+         byte_arr.append(byte)
+   fdst = open(str(sys.argv[2]), 'wb')
+   fdst.write(bytearray(byte_arr))
+   fdst.close()
+
+fsrc.close()
---
+++ ufc2x.py
@@ -0,0 +1,430 @@
+import memory
+from array import array
+
+MPC = 0
+MIR = 0
+
+MAR = 0
+MDR = 0
+PC = 0
+MBR = 0
+X = 0
+Y = 0
+H = 0
+
+N = 0
+Z = 1
+
+BUS_A = 0
+BUS_B = 0
+BUS_C = 0
+
+firmware = array('L',[0]) * 512
+
+#MICROPROGRAMA:
+
+#0: INIT
+firmware[0] = 0b000000000_100_00110101_001000_001_001 
+              #PC = PC + 1; MBR = memory.read_byte(PC) (FETCH); GOTO MBR.
+              
+#2 - add: X = X + mem[address] 
+firmware[2] = 0b000000011_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 3
+firmware[3] = 0b000000100_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 4
+firmware[4] = 0b000000101_000_00010100_000001_000_000
+              #H = MDR; GOTO 5
+firmware[5] = 0b000000000_000_00111100_000100_000_011              
+              #X = H + X; GOTO 0
+
+#6 - mov: memory[address] = X
+firmware[6] = 0b000000111_000_00110101_001000_001_001
+              #PC = PC + 1; FETCH; GOTO 7
+firmware[7] = 0b000001000_000_00010100_100000_000_010
+              #MAR = MBR; GOTO 8
+firmware[8] = 0b000000000_000_00010100_010000_100_011
+              #MDR = X; WRITE_WORD; GOTO 0
+              
+#9 - goto: GOTO address
+firmware[9]  = 0b000001010_000_00110101_001000_001_001
+              #PC = PC + 1; FETCH; GOTO 10
+firmware[10] = 0b000000000_100_00010100_001000_001_010
+              #PC = MBR; FETCH; GOTO MBR
+
+#11 - jz: IF X == 0 GOTO address
+firmware[11] =  0b000001100_001_00010100_000000_000_011
+                #BUS_B = X; IF ALU == 0 GOTO 268 ELSE GOTO 12
+firmware[12] =  0b000000000_000_00110101_001000_000_001
+                #PC = PC + 1; GOTO 0
+firmware[268] = 0b000001001_000_00000000_000000_000_000
+                #GOTO 9
+
+#13 - sub: X = X - mem[address]
+firmware[13] = 0b000001110_000_00110101_001000_001_001
+               #PC = PC + 1; FETCH; GOTO 14
+firmware[14] = 0b000001111_000_00010100_100000_010_010
+               #MAR = MBR; read; GOTO 15 
+firmware[15] = 0b000010000_000_00101000_000001_000_000
+               #H = MDR; GOTO 16
+firmware[16] = 0b000000000_000_01111110_000100_000_011
+               #X = X - H; GOTO 0
+
+#17 - inc: X = X + 1
+firmware[17] = 0b000000000_000_00110101_000100_000_011
+               #X = X + 1; GOTO 0
+
+#18 - dec: X = X - 1
+firmware[18] = 0b000000000_000_00110110_000100_000_011
+               #X = X - 1; GOTO 0
+
+#19 - zera: X = 0
+firmware[19] = 0b000000000_000_00010000_000100_000_011
+               #X = 0; GOTO 0
+
+#20 - add: Y = Y + mem[address] 
+firmware[20] = 0b000010101_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 21
+firmware[21] = 0b000010110_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 22
+firmware[22] = 0b000010111_000_00010100_000001_000_000
+              #H = MDR; GOTO 23
+firmware[23] = 0b000000000_000_00111100_000010_000_100        
+              #Y = H + Y; GOTO 0
+
+#24 - sub: Y = Y - mem[address]
+firmware[24] = 0b000011001_000_00110101_001000_001_001
+               #PC = PC + 1; FETCH; GOTO 25
+firmware[25] = 0b000011010_000_00010100_100000_010_010
+               #MAR = MBR; read; GOTO 26
+firmware[26] = 0b000011011_000_00101000_000001_000_000
+               #H = MDR; GOTO 27
+firmware[27] = 0b000000000_000_01111110_000010_000_100
+               #Y = Y - H; GOTO 0
+
+#28 - inc: Y = Y + 1
+firmware[28] = 0b000000000_000_00110101_000010_000_100
+               #Y = Y + 1; GOTO 0
+
+#29 - dec: Y = Y - 1
+firmware[29] = 0b000000000_000_00110110_000010_000_100
+               #Y = Y - 1; GOTO 0
+
+#30 - zera: Y = 0
+firmware[30] = 0b000000000_000_00010000_000010_000_100
+               #Y = 0; GOTO 0
+
+#31 - mov: memory[address] = Y
+firmware[31] = 0b000100000_000_00110101_001000_001_001
+              #PC = PC + 1; FETCH; GOTO 32
+firmware[32] = 0b000100001_000_00010100_100000_000_010
+              #MAR = MBR; GOTO 33
+firmware[33] = 0b000000000_000_00010100_010000_100_100
+              #MDR = Y; WRITE_WORD; GOTO 0
+
+#34 - jz: IF Y == 0 GOTO address
+firmware[34] =  0b000100011_001_00010100_000000_000_100
+                #BUS_B = Y; IF ALU == 0 GOTO 291 ELSE GOTO 35
+firmware[35] =  0b000000000_000_00110101_001000_000_001
+                #PC = PC + 1; GOTO 0
+firmware[291] = 0b000001001_000_00000000_000000_000_000
+                #GOTO 9
+
+#36 - inc: memory[address] = memory[address] + 1 
+firmware[36] = 0b000100101_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 37
+firmware[37] = 0b000100110_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 38
+firmware[38] = 0b000000000_000_00110101_010000_100_000
+              #MDR = MDR + 1; memory.write_word(MAR); GOTO 0
+
+#39 - dec: memory[address] = memory[address] - 1 
+firmware[39] = 0b000101000_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 40
+firmware[40] = 0b000101001_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 41
+firmware[41] = 0b000000000_000_00110110_010000_100_000
+              #MDR = MDR - 1; memory.write_word(MAR); GOTO 0
+
+#42 - zera: memory[address] = 0
+firmware[42] = 0b000101011_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 40
+firmware[43] = 0b000101100_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 41
+firmware[44] = 0b000000000_000_00010000_010000_100_000
+              #MDR = 0; memory.write_word(MAR); GOTO 0
+
+#45 - madd: memory[address1] = memory[address1] + memory[address2]
+firmware[45] = 0b000101110_000_00110101_001000_000_001
+              #PC = PC + 1; GOTO 46
+firmware[46] = 0b000101111_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 47
+firmware[47] = 0b000110000_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 48
+firmware[48] = 0b000110001_000_00010100_000001_000_000
+              #H = MDR; GOTO 49
+firmware[49] = 0b000110010_000_00110110_001000_001_001
+              #PC = PC - 1; MBR = memory.read_byte(PC) ; GOTO 50
+firmware[50] = 0b000110011_000_00010100_100000_010_010              
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 51
+firmware[51] = 0b000110100_000_00111100_010000_100_000
+              #MDR = MDR + H; memory.write_word(MAR); GOTO 52
+firmware[52] = 0b000000000_000_00110101_001000_000_001
+              #PC = PC + 1
+
+#53 - madd: memory[address] = memory[address] + X
+firmware[53] = 0b000110110_000_00010100_000001_000_011
+              #H = X; GOTO 54
+firmware[54] = 0b000110111_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 55
+firmware[55] = 0b000111000_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 56
+firmware[56] = 0b000000000_000_00111100_010000_100_000
+              #MDR = MDR + X; memory.write_word(MAR); GOTO 0
+
+#57 - madd: memory[address] = memory[address] + Y
+firmware[57] = 0b000111010_000_00010100_000001_000_100
+              #H = Y; GOTO 58
+firmware[58] = 0b000111011_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 59
+firmware[59] = 0b000111100_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 60
+firmware[60] = 0b000000000_000_00111100_010000_000_000
+              #MDR = MDR + Y; GOTO 0
+
+#61 - msub: memory[address1] = memory[address1] - memory[address2]
+firmware[61] = 0b000111110_000_00110101_001000_000_001
+              #PC = PC + 1; GOTO 62
+firmware[62] = 0b000111111_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 63
+firmware[63] = 0b001000000_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 64
+firmware[64] = 0b001000001_000_00010100_000001_000_000
+              #H = MDR; GOTO 65
+firmware[65] = 0b001000010_000_00110110_001000_001_001
+              #PC = PC - 1; MBR = memory.read_byte(PC) ; GOTO 66
+firmware[66] = 0b001000011_000_00010100_100000_010_010              
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 67
+firmware[67] = 0b001000100_000_00111111_010000_100_000
+              #MDR = MDR - H; memory.write_word(MAR); GOTO 68
+firmware[68] = 0b000000000_000_00110101_001000_000_001
+              #PC = PC + 1; GOTO 0
+
+#69 - msub: memory[address] = memory[address] - X
+firmware[69] = 0b001000110_000_00010100_000001_000_011
+              #H = X; GOTO 70
+firmware[70] = 0b001000111_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 71
+firmware[71] = 0b001001000_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 72
+firmware[72] = 0b000000000_000_00111111_010000_100_000
+              #MDR = MDR - X; memory.write_word(MAR); GOTO 0
+
+#73 - msub: memory[address] = memory[address] - Y
+firmware[73] = 0b001001010_000_00010100_000001_000_100
+              #H = Y; GOTO 74
+firmware[74] = 0b001001011_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 75
+firmware[75] = 0b001001100_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 76
+firmware[76] = 0b000000000_000_00111111_010000_000_000
+              #MDR = MDR - Y; GOTO 0
+
+#77 - mmov: memory[address2] = memory[address1]
+firmware[77] = 0b001001110_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 78
+firmware[78] = 0b001001111_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 79
+firmware[79] = 0b001010000_000_00010100_000001_000_000
+              #H = MDR; GOTO 80
+firmware[80] = 0b001010001_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 81
+firmware[81] = 0b001010010_000_00010100_100000_000_010
+              #MAR = MBR; GOTO 82
+firmware[82] = 0b000000000_000_00011000_010000_100_000
+              #MDR = H; write_word(MAR); GOTO 0
+
+#83 - mmov: X = memory[address]
+firmware[83] = 0b001010100_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 84
+firmware[84] = 0b001010101_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 85
+firmware[85] = 0b001010110_000_00010100_000001_000_000
+              #H = MDR; GOTO 86
+firmware[86] = 0b000000000_000_00011000_000100_000_011
+              #X = H; GOTO 0 
+
+#83 - mmov: Y = memory[address]
+firmware[87] = 0b001011000_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 88
+firmware[88] = 0b001011001_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 89
+firmware[89] = 0b001011010_000_00010100_000001_000_000
+              #H = MDR; GOTO 90
+firmware[90] = 0b000000000_000_00011000_000010_000_100
+              #Y = H; GOTO 0              
+
+"""
+#45: IF mem == 0 GOTO address
+firmware[45] = 0b000101110_000_00110101_001000_001_001
+              #PC = PC + 1; MBR = memory.read_byte(PC); GOTO 46
+firmware[46] = 0b000101111_000_00010100_100000_010_010
+              #MAR = MBR; MDR = memory.read_word(MAR); GOTO 47
+firmware[47] = 0b000110000_001_00010100_000000_000_000
+                #BUS_B = MDR; IF ALU == 0 GOTO 304 ELSE GOTO 48
+firmware[48] = 0b000000000_000_00110101_001000_000_001
+                #PC = PC + 1; GOTO 0
+firmware[304] = 0b000001001_000_00000000_000000_000_000
+                #GOTO 9
+"""
+
+#255: HALT
+firmware[255] = 0b00000000000000000000000000000000
+                #HALT
+
+
+
+def read_regs(reg_num):
+   global MDR, PC, MBR, X, Y, H, BUS_A, BUS_B
+    
+   BUS_A = H
+    
+   if reg_num == 0:
+      BUS_B = MDR
+   elif reg_num == 1:
+      BUS_B = PC
+   elif reg_num == 2:
+      BUS_B = MBR
+   elif reg_num == 3:
+      BUS_B = X
+   elif reg_num == 4:
+      BUS_B = Y
+   else:
+      BUS_B = 0
+
+def write_regs(reg_bits):
+   global MAR, MDR, PC, X, Y, H, BUS_C
+ 
+   if reg_bits & 0b100000:
+      MAR = BUS_C
+   if reg_bits & 0b010000:
+      MDR = BUS_C
+   if reg_bits & 0b001000:
+      PC = BUS_C
+   if reg_bits & 0b000100:
+      X = BUS_C
+   if reg_bits & 0b000010:
+      Y = BUS_C
+   if reg_bits & 0b000001:
+      H = BUS_C
+
+def alu(control_bits):
+   global N, Z, BUS_A, BUS_B, BUS_C
+   
+   a = BUS_A
+   b = BUS_B
+   o = 0
+   
+   shift_bits = control_bits & 0b11000000
+   shift_bits = shift_bits >> 6
+
+   control_bits = control_bits & 0b00111111
+   
+   if control_bits == 0b011000:
+      o = a
+   elif control_bits == 0b010100:
+      o = b
+   elif control_bits == 0b011010:
+      o = ~a
+   elif control_bits == 0b101100:
+      o = ~b
+   elif control_bits == 0b111100:
+      o = a + b
+   elif control_bits == 0b111101:
+      o = a + b + 1
+   elif control_bits == 0b111001:
+      o = a + 1
+   elif control_bits == 0b110101:
+      o = b + 1
+   elif control_bits == 0b111111:
+      o = b - a
+   elif control_bits == 0b110110:
+      o = b - 1
+   elif control_bits == 0b111011:
+      o = -a
+   elif control_bits == 0b001100:
+      o = a & b
+   elif control_bits == 0b011100:
+      o = a | b
+   elif control_bits == 0b010000:
+      o = 0
+   elif control_bits == 0b110001:
+      o = 1
+   elif control_bits == 0b110010:
+      o = -1
+   
+   if o == 0:
+      N = 0
+      Z = 1
+   else:
+      N = 1
+      Z = 0
+   
+   if shift_bits == 0b01:
+      o = o << 1
+   elif shift_bits == 0b10:
+      o = o >> 1
+   elif shift_bits == 0b11:
+      o = o << 8
+
+   print(f"a = {a} / b = {b} / o = {o}")
+   BUS_C = o
+    
+def next_instruction(nextadd, jam):
+   global MPC
+   
+   print(f"H = {H} / MBR = {MBR} / MAR = {MAR} / MDR = {MDR} / PC {PC}")
+
+   if jam == 0b000:
+       print(nextadd)
+       MPC = nextadd
+       return
+       
+   if jam & 0b001:
+       nextadd = nextadd | (Z << 8)
+       
+   if jam & 0b010:
+       nextadd = nextadd | (N << 8)
+       
+   if jam & 0b100:
+       nextadd = nextadd | MBR
+
+   print(nextadd)    
+   MPC = nextadd
+
+def memory_io(mem_bits):
+   global PC, MAR, MDR, MBR
+   
+   if mem_bits & 0b001:
+      MBR = memory.read_byte(PC)
+   if mem_bits & 0b010:
+      print("lendo word MAR")
+      MDR = memory.read_word(MAR)
+   if mem_bits & 0b100:
+      memory.write_word(MAR, MDR)
+
+def step():
+   global MIR, MPC
+   
+   MIR = firmware[MPC]
+   
+   if MIR == 0:
+      return False
+   
+   read_regs( MIR & 0b00000000000000000000000000000111 )
+   alu((MIR & 0b00000000000011111111000000000000) >> 12)
+   write_regs( (MIR & 0b00000000000000000000111111000000) >> 6)
+   memory_io( (MIR & 0b00000000000000000000000000111000) >> 3 )
+   next_instruction(MIR >> 23, (MIR & 0b00000000011100000000000000000000) >> 20)
+   
+   return True
+   
